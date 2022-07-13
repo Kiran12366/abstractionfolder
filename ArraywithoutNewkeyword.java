0be698f54683b18ff62bfcf8897e7f009506288c
kiran podey <twinklepodey@gmail.com>
2022-07-13 22:01:10 +0530

    code is added

---
+++ ArraywithoutNewkeyword.java
@@ -0,0 +1,23 @@
+package Array;
+
+public class ArraywithoutNewkeyword {
+
+	public static void main(String[] args) {
+		 
+//		char grade[]= {'A','B','C','D'};
+//		for(int i=0;i<=grade.length-1;i++) {
+//			System.out.println(grade[i]);
+//		}
+//		
+//		int roll_no[]= {1,2,3,4,5};
+//		for(int i=0;i<=roll_no.length-1;i++) {
+//			System.out.println(roll_no[i]);
+//		}
+//	
+		String str[]= {"hello","hi","good","morning"};
+		for(int i=str.length-1;i>=0;i--) {
+			System.out.println(str[i]);
+		}
+	}
+
+}
